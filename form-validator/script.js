850514de1f2d1d5d8fbaeebe9bbedeb2c71eaa55
AlenaNik <alena.nik.digital@gmail.com>
2020-02-23 15:07:51 +0100

    better looking

--- form-validator/script.js
+++ form-validator/script.js
@@ -26,31 +26,23 @@ function isValidEmail(email) {
   }
   
 
+function checkRequired(inputArr) {
+    inputArr.forEach(function(input) {
+        if(input.value.trim() === '') {
+            showError(input, `${getFieldName(input)} is required`)
+        } else {
+            showSuccess(input)
+        }
+    })
+}
+
+function getFieldName(input) {
+    return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
 // EventListeners
 
 form.addEventListener('submit', function(e) {
     e.preventDefault();
-    if(username.value === '') {
-        showError(username, 'Username is required')
-    } else {
-        showSuccess(username)
-    }
-    if(email.value === '') {
-        showError(email, 'Email is required')
-    } else if (!isValidEmail(email.value)) {
-        showError(email, 'Email is not valid')
-    } else {
-        showSuccess(email)
-    }
-
-    if(password.value === '') {
-        showError(password, 'Password is required')
-    } else {
-        showSuccess(password)
-    }
-    if(password2.value === '') {
-        showError(password2, 'Repeat')
-    } else {
-        showSuccess(password2)
-    }
+   
+    checkRequired([username, email, password, password2])
 }) 
