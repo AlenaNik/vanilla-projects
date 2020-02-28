4cb940302e5ba664cc096a74cc08e025a59627bd
AlenaNik <alena.nik.digital@gmail.com>
2020-02-28 12:49:52 +0100

    selected seats

--- movie-seat/script.js
+++ movie-seat/script.js
@@ -5,13 +5,52 @@ const count = document.getElementById('count')
 const total = document.getElementById('total')
 const movieSelect = document.getElementById('movie');
 
-const ticketPrice = Number(movieSelect.value);
+populateUI()
 
+let ticketPrice = Number(movieSelect.value);
+
+
+function setMovieData(movieIndex, moviePrice) {
+    localStorage.setItem('selectedMovieIndex', movieIndex);
+    localStorage.setItem('selectedMoviePrice', moviePrice)
+}
+
+function updateSelectedCount() {
+    const selectedSeats = document.querySelectorAll('.row .seat.selected');
+    
+    // Local Storage
+
+    const seatsIndex = [...selectedSeats].map(seat => 
+         [...seats].indexOf(seat))
+
+   localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex))
+    
+    
+    const selectedSeatsCount = selectedSeats.length;
+    count.innerText = selectedSeatsCount;
+    total.innerText = selectedSeatsCount * ticketPrice;
+}
 
 // event Listeners
 
-container.addEventListener('click', (e) => {
+function populateUI() {
+    const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+    console.log(selectedSeats)
+}
+
+// movie select event
+movieSelect.addEventListener('change', e => {
+    ticketPrice = Number(e.target.value)
+    setMovieData(e.target.selectedIndex, e.target.value)
+    updateSelectedCount();
+})
+
+
+// Seat click event
+container.addEventListener('click', e => {
     if(e.target.classList.contains('seat') && !e.target.classList.contains('occupied')) {
-        e.target.classList.toggle('selected')
+        e.target.classList.toggle('selected');
+
+        updateSelectedCount();
     }
 });
