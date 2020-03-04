3809bc9d46802a4fd236b8bdb6591219d3ce5785
AlenaNik <alena.nik.digital@gmail.com>
2020-03-04 13:01:11 +0100

    functions for video

---
+++ video-player/script.js
@@ -0,0 +1,47 @@
+const video = document.getElementById('video');
+const play = document.getElementById('play');
+const stop = document.getElementById('stop');
+const progress = document.getElementById('progress');
+const timestamp = document.getElementById('timestamp');
+
+
+//Play and pause
+
+function toggleVideoStatus() {
+    video.paused ? video.play() : video.pause()
+}
+
+function updatePlayIcon() {
+    video.paused ? (play.innerHTML = '<i class="fa fa-play fa-2x"></i>')
+    : (play.innerHTML = '<i class="fa fa-pause fa-2x"></i>')
+}    
+
+// update progress & timestamp 
+function updateProgress() {
+    return true;
+}
+
+// set video progress
+function setVideoProgress() {
+    return true;
+}
+
+// stop video
+
+function stopVideo() {
+    video.currentTime = 0;
+    video.pause();
+}
+
+//eventListeners
+
+video.addEventListener('click', toggleVideoStatus);
+video.addEventListener('pause', updatePlayIcon);
+video.addEventListener('play', updatePlayIcon);
+video.addEventListener('timeupdate', updateProgress);
+
+play.addEventListener('click', toggleVideoStatus);
+
+stop.addEventListener('click', stopVideo);
+
+progress.addEventListener('change', setVideoProgress);
