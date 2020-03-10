2b10bdbc3f83ca1fb151a46bd6285ae4b4d59938
AlenaNik <alena.nik.digital@gmail.com>
2020-03-10 22:31:08 +0100

    output persons

--- array-methods/script.js
+++ array-methods/script.js
@@ -7,11 +7,44 @@ const showBtn = document.getElementById('show-millionaire')
 const calculateWealthBtn = document.getElementById('calculate-wealth')
 
 
+
+getRandomUser()
+getRandomUser()
+getRandomUser()
+
 let data = [];
 async function getRandomUser() {
     const res = await fetch('https://randomuser.me/api');
     const data = await res.json()  
-    console.log(data)
+    const user = data.results[0]
+    
+    const newUser = {
+        name: `${user.name.first}
+               ${user.name.last}`,
+        money: Math.floor(Math.random() * 1000000)       
+    };
+    addData(newUser)
 }
 
-getRandomUser()
+function addData(obj) {
+    data.push(obj)
+
+    updateDOM();
+}
+
+
+function updateDOM(providedData = data) {
+    main.innerHTML = '<h2><strong>Person</strong>Wealth</h2>'
+
+    providedData.forEach(function(person) {
+        const personElement = document.createElement('div');
+        personElement.classList.add('person')
+        personElement.innerHTML = `<span>
+                                ${person.name}
+                                   </span>
+                                ${person.money} $   
+                                   `
+        main.appendChild(personElement)                                       
+    });
+
+}
